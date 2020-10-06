b86937596a515d3024b27a1b7475799197e3668e
Niirn <niirn-dev@users.noreply.github.com>
2020-10-06 13:07:52 +0300

    create a window (no message processing)

--- hw3d/WinMain.cpp
+++ hw3d/WinMain.cpp
@@ -7,5 +7,37 @@ int WINAPI wWinMain(
 	_In_		int			nCmdShow
 )
 {
+	// register window class
+	const auto pClassName = "hw3dWndClass";
+
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize		 = sizeof( wc );
+	wc.style		 = CS_OWNDC;
+	wc.lpfnWndProc	 = DefWindowProc;
+	wc.cbClsExtra	 = 0;
+	wc.cbWndExtra	 = 0;
+	wc.hInstance	 = hInstance;
+	wc.hIcon		 = nullptr;
+	wc.hCursor		 = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName	 = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm		 = nullptr;
+	RegisterClassEx( &wc );
+
+	// create a window
+	HWND hWnd = CreateWindowEx( 
+		0,
+		pClassName,
+		"HW3D Window",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200,200,800,600,
+		nullptr,nullptr,
+		hInstance,nullptr
+	);
+	// show window
+	ShowWindow( hWnd,SW_SHOW );
+
+	while ( true );
 	return 0;
 }
