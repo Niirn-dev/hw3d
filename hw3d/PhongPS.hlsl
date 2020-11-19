a76fe22a16251a0a6d0ddd6e838844a01bfd93c8
Niirn <dreammagica@gmail.com>
2020-11-19 11:51:33 +0300

    update shaders to use textures

--- hw3d/PhongPS.hlsl
+++ hw3d/PhongPS.hlsl
@@ -11,13 +11,14 @@ cbuffer LightCBuf
 
 cbuffer ObjectCBuf
 {
-	float3 materialColor;
 	float specularIntensity;
 	float specularPower;
 };
 
+Texture2D tex;
+SamplerState smplr;
 
-float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_Target
+float4 main( float3 worldPos : Position,float3 n : Normal,float2 tc : Texcoord ) : SV_Target
 {
 	// fragment to light vector data
 	const float3 vToL = lightPos - worldPos;
@@ -33,5 +34,5 @@ float4 main( float3 worldPos : Position,float3 n : Normal ) : SV_Target
 	// calculate specular intensity based on angle between viewing vector and reflection vector, narrow with power function
 	const float3 specular = att * (diffuseColor * diffuseIntensity) * specularIntensity * pow( max( 0.0f,dot( normalize( -r ),normalize( worldPos ) ) ),specularPower );
 	// final color
-	return float4(saturate( (diffuse + ambient + specular) * materialColor ),1.0f);
+	return float4(saturate( (diffuse + ambient + specular ) * (float3)tex.Sample(smplr, tc)), 1.0f);
 }
