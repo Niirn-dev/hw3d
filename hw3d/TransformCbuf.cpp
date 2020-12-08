e5ce3481163c8f1f34afeb65d9dbab42eb2e2fde
Niirn <dreammagica@gmail.com>
2020-12-08 17:23:24 +0300

    refactor transformcbuf for better extencibility

--- hw3d/TransformCbuf.cpp
+++ hw3d/TransformCbuf.cpp
@@ -2,29 +2,37 @@
 
 namespace Bind
 {
-	TransformCbuf::TransformCbuf( Graphics& gfx,const Drawable& parent,UINT slot )
+	TransformCbuf::TransformCbuf( Graphics& gfx,const Drawable& parent,std::optional<UINT> slot )
 		:
 		parent( parent )
 	{
 		if( !pVcbuf )
 		{
-			pVcbuf = std::make_unique<VertexConstantBuffer<Transforms>>( gfx,slot );
+			pVcbuf = std::make_unique<VertexConstantBuffer<Transforms>>( gfx,slot.value_or( 0u ) );
 		}
 	}
 
 	void TransformCbuf::Bind( Graphics& gfx ) noexcept
 	{
+		UpdateBindImpl( gfx,GetTransforms( gfx ) );
+	}
+
+	void TransformCbuf::UpdateBindImpl( Graphics& gfx,Transforms transforms ) noexcept
+	{
+		pVcbuf->Update( gfx,transforms );
+		pVcbuf->Bind( gfx );
+	}
+
+	TransformCbuf::Transforms TransformCbuf::GetTransforms( Graphics& gfx ) noexcept
+	{
 		const auto modelView = parent.GetTransformXM() * gfx.GetCamera();
-		const Transforms tf =
-		{
+		return {
 			DirectX::XMMatrixTranspose( modelView ),
 			DirectX::XMMatrixTranspose(
 				modelView *
 				gfx.GetProjection()
 			)
 		};
-		pVcbuf->Update( gfx,tf );
-		pVcbuf->Bind( gfx );
 	}
 
 	std::unique_ptr<VertexConstantBuffer<TransformCbuf::Transforms>> TransformCbuf::pVcbuf;
--- hw3d/TransformCbuf.h
+++ hw3d/TransformCbuf.h
@@ -2,20 +2,24 @@
 #include "ConstantBuffers.h"
 #include "Drawable.h"
 #include <DirectXMath.h>
+#include <optional>
 
 namespace Bind
 {
 	class TransformCbuf : public Bindable
 	{
-	private:
+	protected:
 		struct Transforms
 		{
 			DirectX::XMMATRIX modelViewProj;
 			DirectX::XMMATRIX model;
 		};
 	public:
-		TransformCbuf( Graphics& gfx,const Drawable& parent,UINT slot = 0u );
+		TransformCbuf( Graphics& gfx,const Drawable& parent,std::optional<UINT> slot = {} );
 		void Bind( Graphics& gfx ) noexcept override;
+	protected:
+		void UpdateBindImpl( Graphics& gfx,Transforms transforms ) noexcept;
+		Transforms GetTransforms( Graphics& gfx ) noexcept;
 	private:
 		static std::unique_ptr<VertexConstantBuffer<Transforms>> pVcbuf;
 		const Drawable& parent;
